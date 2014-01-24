61a827ff1667268586506b9c9049a4a953ca02c3
ibrahim cereci <ibrahim.cereci@gmail.com>
2014-01-24 23:40:10 +0200

    First Upload. Still 2D infinite runner. Character is borrowed. Sprites will be changed.

---
+++ Assets/Sample Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class Platformer2DUserControl : MonoBehaviour 
+{
+	private PlatformerCharacter2D character;
+    private bool jump;
+
+
+	void Awake()
+	{
+		character = GetComponent<PlatformerCharacter2D>();
+	}
+
+    void Update ()
+    {
+        // Read the jump input in Update so button presses aren't missed.
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+    }
+
+	void FixedUpdate()
+	{
+		// Read the inputs.
+
+		// Pass all parameters to the character control script.
+		character.Move( 1, false , jump );
+
+        // Reset the jump input once it has been used.
+	    jump = false;
+	}
+}
---
+++ Assets/Scripts/CameraFlw.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFlw : MonoBehaviour {
+
+	public Transform player;
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x + 6, 5, -10);
+	}
+}

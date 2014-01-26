f99779cba8834db67064b444e4ceb5c885231f52
ibrahim cereci <ibrahim.cereci@gmail.com>
2014-01-26 13:32:25 +0200

    Platform Complete.. Boss Battle left.

---
+++ Assets/Scripts/DisplayIncBossMessage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayIncBossMessage : MonoBehaviour {
+	public GUISkin gSkin;
+	public bool display=false;
+
+	void OnGUI(){
+		if (display) 
+		{
+			GUI.skin = gSkin;
+			GUI.Label (new Rect (Screen.width/2-100, Screen.height/2-150, 300, 200), "Epic Boss Battle\nIncoming!!1");
+			//GUI.contentColor = Color.red;
+			//GUI.Label (new Rect (Screen.width/2+50, Screen.height/2-50, 300, 200), "Literally!");
+			
+			//GUI.Label (new Rect (10, 10, 100, 30), "We don't see things\nas they are, we see\nthem as we are.");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		display = true;
+		}
+
+}
---
+++ Assets/Scripts/GoToBoss.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToBoss : MonoBehaviour {
+	
+	
+	//public Renderer []texts=new Renderer[4];
+	
+
+	
+	void OnTriggerEnter2D(Collider2D other){
+		
+		//GetComponent<DisplayMessage> ().display = true;
+		
+		//Renderer[] texts = textObject.GetComponentsInChildren<MeshRenderer>();
+		
+		//foreach (MeshRenderer item in texts) {
+		//item.GetComponent<MeshRenderer>().enabled=true;
+		
+		
+		
+		//Debug.Log("item ");
+		//		}
+		
+		Application.LoadLevel(7);
+		return;
+	}
+}
